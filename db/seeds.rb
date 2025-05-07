5bb04b1441402820f3369a9dd056c676400a66b8
Harriet Anderson <harriet.correspondence@gmail.com>
2025-05-07 15:25:47 +0100

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+# ["chinese", "italian", "japanese", "french", "belgian"]
+puts "Creating restaurants..."
+Restaurant.create!(name: "My Favourite Sushi", address: "7 Boundary St, London E2 7JE", category: "japanese")
+puts "Created Dishoom"
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian")
+puts "Created Pizza East"
+Restaurant.create!(name: "Lornas Patisserie", address: "21 Wetherden Street, London E17 8EH", category: "french")
+puts "Created Lornas Patisserie"
+Restaurant.create!(name: "Chinese Out There", address: "6A The High St, London SE1 6PQ", category: "chinese")
+puts "Created Chinese Out Theret"
+Restaurant.create!(name: "Brussels To Go", address: "13a North Street, London N12 3AB", category: "belgian")
+puts "Created Brussels To Go"
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
